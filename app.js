3576601c455c4d04a85e3bb6b0e9ff423e513587
guangwei <guangwei@sogou-inc.com>
2021-08-04 11:51:31 +0800

    fix: deploy registration port

--- app.js
+++ app.js
@@ -5,27 +5,6 @@ const { reqLog, errorHandler } = require('./middleware')
 const { getHandler } = require('./handler')
 
 
-
-const handler = (render) => {
-    return async (ctx) => {
-        const { url, script, cmd } = ctx.request.body
-        console.log((new Date()).toLocaleString(), " => ", url)
-
-        let injectFunc
-        if (cmd) {
-            switch (cmd.type) {
-                case "login":
-                    injectFunc = login(cmd)
-                    break
-            }
-        } else {
-            injectFunc = new AsyncFunction("url", "page", "params", "const {addHeader, setCookie} = params;" + script)
-        }
-
-        ctx.body = await render(url, injectFunc)
-    }
-}
-
 const serve = async () => {
     const app = new Koa()
     const router = new Router()
